0769fa40f5a3578566f166670b34f87497bdcc96
Marlon <marlongarciab@hotmail.com>
2023-03-20 16:36:45 +0100

    first commit

---
+++ Wordle/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  Wordle
+//
+//  Created by Marlon Garcia-Bermejo on 2023-03-20.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+
+}
--- Wordle/ViewController.swift
+++ Wordle/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let segueIdgoToGame = "goToGame"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBAction func startGameButton(_ sender: UIButton) {
+        performSegue(withIdentifier: segueIdgoToGame, sender: self)
+    }
+    
 }
 
